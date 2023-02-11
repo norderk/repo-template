51e6d174245147ab8a1321547239d8b0134a19bc
Erik Nordqvist <nordqvist.eriken@gmail.com>
2023-02-11 19:42:19 +0100

    add linter to commit gate

---
+++ test.py
@@ -0,0 +1,14 @@
+"""
+Test file for github-actions super-linter
+"""
+
+
+def add(x: int, y: int) -> int:
+    """
+    This is a function
+    """
+    return x + y
+
+
+if __name__ == "__main__":
+    print(add(1, 2))
